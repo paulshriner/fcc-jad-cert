19b78db3f0db35776df879ad70a1477dca8ebf69
paulshriner <shrinerpaul@gmail.com>
2024-01-08 13:58:50 -0500

    functionality for cash register

---
+++ cash-register/script.js
@@ -0,0 +1,143 @@
+// Face values for denominations
+const amounts = [
+    ["PENNY", 0.01], 
+    ["NICKEL", 0.05], 
+    ["DIME", 0.10], 
+    ["QUARTER", 0.25], 
+    ["ONE", 1.00], 
+    ["FIVE", 5.00], 
+    ["TEN", 10.00], 
+    ["TWENTY", 20.00], 
+    ["ONE HUNDRED", 100.00]    
+]
+
+// Store elements from the page
+const cash = document.getElementById("cash");
+const total = document.getElementById("total");
+const cidPenny = document.getElementById("cid-penny");
+const cidNickel = document.getElementById("cid-nickel");
+const cidDime = document.getElementById("cid-dime");
+const cidQuarter = document.getElementById("cid-quarter");
+const cidOne = document.getElementById("cid-one");
+const cidFive = document.getElementById("cid-five");
+const cidTen = document.getElementById("cid-ten");
+const cidTwenty = document.getElementById("cid-twenty");
+const cidOneHundred = document.getElementById("cid-onehundred");
+const changeDue = document.getElementById("change-due");
+
+// Price and cid, can change these for testing
+let price = 19.5;
+let cid = [
+    ["PENNY", 0.5], 
+    ["NICKEL", 0], 
+    ["DIME", 0], 
+    ["QUARTER", 0], 
+    ["ONE", 0], 
+    ["FIVE", 0], 
+    ["TEN", 0], 
+    ["TWENTY", 0], 
+    ["ONE HUNDRED", 0]
+];
+
+// Updates cid values on the page
+const updateCid = () => {
+    for (let i = 0; i < cid.length; ++i) {
+        switch (cid[i][0]) {
+            case "PENNY":
+                cidPenny.textContent = `Pennies: $${cid[i][1]}`;
+                break;
+            case "NICKEL":
+                cidNickel.textContent = `Nickels: $${cid[i][1]}`;
+                break;
+            case "DIME":
+                cidDime.textContent = `Dimes: $${cid[i][1]}`;
+                break;
+            case "QUARTER":
+                cidQuarter.textContent = `Quarters: $${cid[i][1]}`;
+                break;
+            case "ONE":
+                cidOne.textContent = `Ones: $${cid[i][1]}`;
+                break;
+            case "FIVE":
+                cidFive.textContent = `Fives: $${cid[i][1]}`;
+                break;
+            case "TEN":
+                cidTen.textContent = `Tens: $${cid[i][1]}`;
+                break;
+            case "TWENTY":
+                cidTwenty.textContent = `Twenties: $${cid[i][1]}`;
+                break;
+            case "ONE HUNDRED":
+                cidOneHundred.textContent = `Hundreds: $${cid[i][1]}`;
+                break;
+        }
+    }
+};
+
+const calculateChange = () => {
+    // Result to store in change-due
+    const result = [
+        ["Status", ""],
+        ["PENNY", 0.00], 
+        ["NICKEL", 0.00], 
+        ["DIME", 0.00], 
+        ["QUARTER", 0.00], 
+        ["ONE", 0.00], 
+        ["FIVE", 0.00], 
+        ["TEN", 0.00], 
+        ["TWENTY", 0.00], 
+        ["ONE HUNDRED", 0.00]
+    ];
+    
+    // Check if customer has enough change
+    if (cash.valueAsNumber < price) {
+        alert("Customer does not have enough money to purchase the item");
+        return;
+    } else if (cash.valueAsNumber === price) {
+        changeDue.innerHTML = `<p>No change due - customer paid with exact cash</p>`;
+        return;
+    }
+
+    // Calculate change due
+    let change = cash.valueAsNumber - price;
+
+    // Find change values
+    for (let i = cid.length - 1; i >= 0; --i) {
+        while (change >= amounts[i][1] && cid[i][1] >= amounts[i][1]) {
+            // Solution to percision error
+            // Source: https://stackoverflow.com/questions/15762768/javascript-math-round-to-two-decimal-places
+            cid[i][1] = Math.round((cid[i][1] - amounts[i][1]) * 100) / 100;
+            result[i + 1][1] = Math.round((result[i + 1][1] + amounts[i][1]) * 100) / 100;
+            change = Math.round((change - amounts[i][1]) * 100) / 100;
+        }
+    }
+
+    // Update status of cid
+    if (change !== 0) {
+        result[0][1] = "INSUFFICIENT_FUNDS"
+    } else {
+        for (let i = 0; i < cid.length; ++i) {
+            if (cid[i][1] !== 0) {
+                result[0][1] = "OPEN";
+                break;
+            }
+            result[0][1] = "CLOSED";
+        }
+    }
+
+    // Update change due content on page
+    changeDue.innerHTML = `<p>${result[0][0]}: ${result[0][1]}</p>`;
+    if (result[0][1] !== "INSUFFICIENT_FUNDS") {
+        for (let i = result.length - 1; i >= 0; --i) {
+            if (result[i][1] > 0) {
+                changeDue.innerHTML += `<p>${result[i][0]}: ${result[i][1]}</p>`
+            }
+        }
+    }
+
+    updateCid();
+}
+
+// Initial values when page first loads
+total.textContent = `Total: $${price}`;
+updateCid();
